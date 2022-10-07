f4368ba3420bff2b184c73817722ff5b2a2a3dcf
Sectly <67114413+Sectly@users.noreply.github.com>
2022-10-07 09:20:45 +0200

    1.4.5

--- utils.js
+++ utils.js
@@ -6,84 +6,86 @@ let STORAGE_CATALOG = './data';
 let STORAGE_PATH = STORAGE_CATALOG + '/data.sectdb';
 
 function updateStoragePath(path) {
-    if (STORAGE_PATH == "./data/data.sectdb") {
-      STORAGE_PATH = path + '.sectdb'
-    } else {
-      STORAGE_PATH = STORAGE_CATALOG + '/data.sectdb'
-    }
+  if (path) {
+    STORAGE_PATH = path;
+  } else {
+    STORAGE_PATH = STORAGE_CATALOG + '/data.sectdb';
+  }
+
+  console.log(STORAGE_PATH);
 }
 
 function config(options) {
-    STORAGE_CATALOG = options.catalog;
-    updateStoragePath(options.path);
+  STORAGE_CATALOG = options.catalog;
+  updateStoragePath(`${STORAGE_CATALOG}/${options.name}.sectdb`);
 }
 
 class JsonStorage {
-    static _open() {
-        if (!fs.existsSync(STORAGE_CATALOG)) {
-            fs.mkdirSync(STORAGE_CATALOG);
-            fs.writeFileSync(STORAGE_PATH, '{}');
-        } else if (!fs.existsSync(STORAGE_PATH)) {
-            fs.writeFileSync(STORAGE_PATH, '{}');
-        }
-
-        return fs.readFileSync(STORAGE_PATH).toString();
+  static _open() {
+    if (!fs.existsSync(STORAGE_CATALOG)) {
+      fs.mkdirSync(STORAGE_CATALOG);
+      fs.writeFileSync(STORAGE_PATH, '{}');
+    } else if (!fs.existsSync(STORAGE_PATH)) {
+      fs.writeFileSync(STORAGE_PATH, '{}');
     }
 
-    static _write(data) {
-        fs.writeFileSync(STORAGE_PATH, data);
-    }
+    return fs.readFileSync(STORAGE_PATH).toString();
+  }
 
-    static get(key) {
-        const content = this._open();
-        try {
-            const json = JSON.parse(content);
-            return json[key];
-        } catch (e) {
-            throw Error(e);
-        }
-    }
+  static _write(data) {
+    fs.writeFileSync(STORAGE_PATH, data);
+  }
 
-    static set(key, value) {
-        const content = this._open();
-        try {
-            const json = JSON.parse(content);
-            json[key] = value;
-            this._write(JSON.stringify(json));
-        } catch (e) {
-            throw Error(e);
-        }
+  static get(key) {
+    const content = this._open();
+    try {
+      const json = JSON.parse(content);
+      return json[key];
+    } catch (e) {
+      throw Error(e);
     }
+  }
 
-    static delete(key) {
-        const content = this._open();
-        try {
-            const json = JSON.parse(content);
-            delete json[key];
-            this._write(JSON.stringify(json));
-        } catch (e) {
-            throw Error(e);
-        }
+  static set(key, value) {
+    const content = this._open();
+    try {
+      const json = JSON.parse(content);
+      json[key] = value;
+      this._write(JSON.stringify(json));
+    } catch (e) {
+      throw Error(e);
     }
+  }
 
-    static getAllKeys() {
-        const content = this._open();
-        try {
-            const json = JSON.parse(content);
-            return Object.keys(json);
-        } catch (e) {
-            throw Error(e);
-        }
+  static delete(key) {
+    const content = this._open();
+    try {
+      const json = JSON.parse(content);
+      delete json[key];
+      this._write(JSON.stringify(json));
+    } catch (e) {
+      throw Error(e);
     }
+  }
 
-    static clearAll() {
-        this.getAllKeys().forEach(key => {
-            this.delete(key);
-        });
+  static getAllKeys() {
+    const content = this._open();
+    try {
+      const json = JSON.parse(content);
+      return Object.keys(json);
+    } catch (e) {
+      throw Error(e);
     }
+  }
+
+  static clearAll() {
+    this.getAllKeys().forEach(key => {
+      this.delete(key);
+    });
+  }
 }
 
 module.exports = {
-    JsonStorage,
-    config
+  JsonStorage,
+  config
 }
